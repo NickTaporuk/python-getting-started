1fd0b1475fa5d710965115fefe89149cc572fe5c
Nicholay Kuropatkin <nictaporuk@yandex.ru>
2016-12-05 10:28:29 +0200

    added modules in settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apartment'
+    'apartment',
+    'markdown_deux',
+    'pagedown',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
